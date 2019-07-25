4bc1a3795c2a64a5aa17f6c7992530fdad61f2c5
José Luiz Coe <joseluizcoe@gmail.com>
2019-07-25 16:43:00 -0300

    Adding redux Provider with promise middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware  from 'redux-promise';
+import reducers from './reducers';
+
 import App from './components/App/App';
+
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const withMiddleware = applyMiddleware(promiseMiddleware);
+const storeWithMiddleware = withMiddleware(createStore);
+
+ReactDOM.render(
+  <Provider store={storeWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  ,document.getElementById('root')
+);
